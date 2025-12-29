a5eef8803b02abb2c413c1741b319b9a4749f86a
HJohnRoss <rossjohn936@gmail.com>
2025-12-29 15:40:07 -0800

    minor changes

--- src/LCB_Clone.Api/Controllers/LegislatorsController.cs
+++ src/LCB_Clone.Api/Controllers/LegislatorsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LCB_Clone.Api.Controllers;
 
 [ApiController]
-[Route("api[controller]")]
+[Route("api/[controller]")]
 public class LegislatorsController(ILegislatorService legislatorService) : ControllerBase
 {
 	private readonly ILegislatorService _legislatorService = legislatorService;
